013a1d68201ebb22456965959e7dde67e726392f
Valerio Faiuolo <dev@olosnet.com>
2022-06-22 06:34:20 +0200

    First commit

---
+++ indi-sv305/indi_sv305_ccd.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="CCDs">
+        <device label="SVBONY SV305" mdpd="true" manufacturer="SVBONY">
+                <driver name="SVBONY SV305">indi_sv305_ccd</driver>
+                <version>@SV305_VERSION_MAJOR@.@SV305_VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
